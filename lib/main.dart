6e5c18b4a64f1335ed0018d89f89e188a3fc4c20
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-10-10 09:59:30 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'pages/search_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        brightness: Brightness.dark,
+      ),
+      home: const SearchPage()
+    );
+  }
+}
